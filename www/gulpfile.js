dfc5b2d89bc61762033b3c13751386226cd8842f
Justin Menkveld <justin@menklab.com>
2016-01-15 16:14:05 -0500

    added gulp to build

---
+++ www/gulpfile.js
@@ -0,0 +1,112 @@
+var gulp = require('gulp'),
+    path = require('path'),
+    plumber = require('gulp-plumber'),
+    gulp_watch = require('gulp-watch'),
+    gulp_concat = require('gulp-concat'),
+    gulp_uglifycss = require('gulp-uglifycss'),
+    gulp_uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    jshint_stylish = require('jshint-stylish'),
+    minimist = require('minimist'),
+    run_sequence = require('run-sequence'),
+    gulp_open = require('gulp-open'),
+    gulp_sourcemaps = require('gulp-sourcemaps'),
+    fs = require('fs'),
+    livereload = require('gulp-livereload');
+
+
+var options = minimist(process.argv.slice(2), {
+    string: 'env',
+    default: {env: process.env.NODE_ENV || 'dev'}
+});
+
+var sourcemapsWriteOptions = {
+    includeContent: false,
+    sourceRoot: function (file) {
+        return path.dirname(
+            path.relative('maps', file.path))
+            .replace(/\\/g, '/');
+    }
+};
+
+var jshintOptions = {
+    debug: options.env === 'dev',
+    devel: options.env === 'dev'
+};
+
+gulp.task('build:css', function () {
+    return gulp.src([
+            'app/src/**/*.css'
+        ])
+        .pipe(gulp_sourcemaps.init())
+        .pipe(gulp_concat('style.min.css'))
+        .pipe(gulp_uglifycss())
+        .pipe(gulp_sourcemaps.write('../maps'))
+        .pipe(gulp.dest('app/'))
+        .pipe(livereload());
+});
+
+gulp.task('build:js', function () {
+    return gulp.src(['app/src/**/*.js'], {base: 'js'})
+        .pipe(jshint(jshintOptions))
+        .pipe(jshint.reporter(jshint_stylish))
+        //.pipe(jshint.reporter('fail')) // only enable if build needs to fail on bad jshint
+        .pipe(gulp_sourcemaps.init())
+        .pipe(gulp_concat('app.min.js'))
+        .pipe(gulp_uglify())
+        .pipe(gulp_sourcemaps.write('../maps'))
+        .pipe(gulp.dest('app/'))
+        .pipe(livereload());
+});
+
+gulp.task('build:vendor:js', function () {
+    return gulp.src([
+            'bower_components/sjcl/sjcl.js',
+            'bower_components/jszip/dist/jszip.js',
+            'bower_components/dropzone/dist/dropzone.js'
+        ], {base: 'js'})
+        .pipe(gulp_sourcemaps.init())
+        .pipe(gulp_concat('vendor.min.js'))
+        .pipe(gulp_uglify())
+        .pipe(gulp_sourcemaps.write('../maps'))
+        .pipe(gulp.dest('app/'));
+});
+
+gulp.task('build:vendor:css', function () {
+    gulp.src([
+        'bower_components/dropzone/dist/dropzone.css',
+        'bower_components/dropzone/dist/basic.css'
+    ])
+        .pipe(gulp_concat('vendor.css'))
+        .pipe(gulp.dest('app/src/styles/'));
+});
+
+gulp.task('open:dev', function () {
+    gulp.src('')
+        .pipe(gulp_open({uri: 'http://localhost:8080/'}));
+});
+
+gulp.task('dev', [], function () {
+    run_sequence('build:vendor:js');
+    run_sequence('build:vendor:css');
+    run_sequence('build:js');
+    run_sequence('build:css');
+    run_sequence('watch');
+    run_sequence('open:dev');
+});
+
+
+gulp.task('watch', [], function () {
+    livereload.listen({port: 35729});
+    gulp_watch('app/src/**/*.css', function () {
+        run_sequence('build:css');
+    });
+    gulp_watch('app/js/**/*.js', function () {
+        run_sequence('build:js');
+    });
+});
+
+gulp.task('default', function () {
+
+
+});
